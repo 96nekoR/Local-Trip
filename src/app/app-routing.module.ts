28f8c61b8591d9dd091218f9f459d6fa331904d8
96nekoR <seed.kira0807@gmail.com>
2018-11-07 14:36:24 +0900

    change root page signin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'signin', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'signin', loadChildren: './signin/signin.module#SigninPageModule' },
 ];
